514123828dd2565f0d50fed1f698d25d1f662f84
Ryan <iperish.x@gmail.com>
2023-07-16 16:33:46 +0200

    add model for jobs

---
+++ src/SilverForest.Common/Models/SkillJob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilverForest.Common.Models;
+
+public class SkillJob
+{
+    public Guid Guid { get; set; }
+    public int PlayerId { get; set; }
+    public Skill Skill { get; set; }
+    public TimeSpan ExpireTime { get; set; }
+
+}
