551b967676781fcf8dc8b9b260e76de018b9583b
Ryan <iperish.x@gmail.com>
2023-07-16 03:31:23 +0200

    add endpoint for forestry

---
+++ src/SilverForest.Api/Controllers/ForestryController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SilverForest.Api.Abstraction;
+using SilverForest.Common.Requests;
+using SilverForest.Infrastructure.Redis.Abstraction;
+
+namespace SilverForest.Api.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+[Authorize]
+public class ForestryController : SilverForestApiControllerBase
+{
+    private readonly ILogger<ForestryController> _logger;
+    private readonly IRedisJobCache _cache;
+
+    public ForestryController(ILogger<ForestryController> logger, IRedisJobCache cache)
+    {
+        _logger = logger;
+        _cache = cache;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Woodcut([FromBody]ForestryWoodcutRequest req)
+    {
+        if (req == null)
+        {
+            return BadRequest();
+        }
+
+        await _cache.AddJob(Guid.NewGuid().ToString(), req.Id);
+        return Ok();
+    }
+}
